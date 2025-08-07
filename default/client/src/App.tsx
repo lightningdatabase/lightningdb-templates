57b115fb61a916538a6abe2365513e306695dcd9
Edward Louth <edward@louths.com>
2025-08-07 11:43:30 +0100

    Update to latest playground

--- default/client/src/App.tsx
+++ default/client/src/App.tsx
@@ -1,39 +1,14 @@
 import { Container } from "@mui/material"
 import Welcome from "./Welcome"
-import UsersList from "./users/UsersList"
 import Section from "./Section"
-import CreateUser from "./users/CreateUser"
-import DeleteUser from "./users/DeleteUser"
-import UpdateUser from "./users/UpdateUser"
-import UpdatePost from "./posts/UpdatePost"
 import { Playground } from "@lightningdb/playground"
 import types from "../lightningdb/schema.ts?raw"
 
 const App: React.FC = () => (
   <Container maxWidth="lg" sx={{ py: 4 }}>
     <Welcome />
-    <Section title="Users List" defaultExpanded>
-      <UsersList />
-    </Section>
-    <Section title="Create User">
-      <CreateUser />
-    </Section>
-    <Section title="Update User">
-      <UpdateUser />
-    </Section>
-    <Section title="Delete User">
-      <DeleteUser />
-    </Section>
-    <Section title="Update Post">
-      <UpdatePost />
-    </Section>
     <Section title="Playground">
-      <Playground
-        types={types}
-        initialCode={`useQuery({
-  users: {},
-})`}
-      />
+      <Playground types={types} />
     </Section>
   </Container>
 )
--- default/client/src/Section.tsx
+++ default/client/src/Section.tsx
@@ -1,42 +1,5 @@
 import type { ReactNode } from "react"
-import { styled, Typography } from "@mui/material"
-import type { AccordionProps } from "@mui/material/Accordion"
-import type { AccordionSummaryProps } from "@mui/material/AccordionSummary"
-import MuiAccordion from "@mui/material/Accordion"
-import MuiAccordionSummary, {
-  accordionSummaryClasses,
-} from "@mui/material/AccordionSummary"
-import MuiAccordionDetails from "@mui/material/AccordionDetails"
-import { ExpandMore } from "@mui/icons-material"
-
-const Accordion = styled((props: AccordionProps) => (
-  <MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-  border: `1px solid ${theme.palette.divider}`,
-  "&::before": {
-    display: "none",
-  },
-}))
-
-const AccordionSummary = styled((props: AccordionSummaryProps) => (
-  <MuiAccordionSummary
-    expandIcon={<ExpandMore sx={{ fontSize: "0.9rem" }} />}
-    {...props}
-  />
-))(({ theme }) => ({
-  backgroundColor: "rgba(0, 0, 0, .01)",
-  [`& .${accordionSummaryClasses.content}`]: {
-    marginLeft: theme.spacing(1),
-  },
-  ...theme.applyStyles("dark", {
-    backgroundColor: "rgba(255, 255, 255, .05)",
-  }),
-}))
-
-const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-  padding: theme.spacing(0),
-  borderTop: "1px solid rgba(0, 0, 0, .125)",
-}))
+import { Box, Paper, Typography } from "@mui/material"
 
 type SectionProps = {
   title: string
@@ -44,21 +7,13 @@ type SectionProps = {
   defaultExpanded?: boolean
 }
 
-const Section: React.FC<SectionProps> = ({
-  title,
-  children,
-  defaultExpanded = false,
-}) => (
-  <Accordion defaultExpanded={defaultExpanded} sx={{ mt: 2 }}>
-    <AccordionSummary
-      expandIcon={<ExpandMore />}
-      aria-controls="panel1-content"
-      id="panel1-header"
-    >
+const Section: React.FC<SectionProps> = ({ title, children }) => (
+  <Paper sx={{ mt: 2, border: 1, borderColor: "divider" }} elevation={0}>
+    <Box sx={{ p: 2 }}>
       <Typography component="span">{title}</Typography>
-    </AccordionSummary>
-    <AccordionDetails>{children}</AccordionDetails>
-  </Accordion>
+    </Box>
+    <Box sx={{ borderTop: "1px solid rgba(0, 0, 0, .125)" }}>{children}</Box>
+  </Paper>
 )
 
 export default Section
